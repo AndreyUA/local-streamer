30ad62a1844043ab04f2e8850ec99645e14cbc66
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-15 23:59:56 +0200

    dynamic video file name

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,22 @@
 import { Controller, Get, StreamableFile } from '@nestjs/common';
 import { createReadStream, ReadStream } from 'fs';
+import { readdir } from 'fs/promises';
 import { join } from 'path';
 
 @Controller()
 export class AppController {
   @Get('/video')
-  getVideo(): StreamableFile {
-    const fileStream: ReadStream = createReadStream(
-      join(process.cwd(), 'video/password.webm'),
-    );
+  async getVideo(): Promise<StreamableFile> {
+    try {
+      const files = await readdir(join(process.cwd(), 'video'));
 
-    return new StreamableFile(fileStream);
+      const fileStream: ReadStream = createReadStream(
+        join(process.cwd(), `video/${files[1]}`),
+      );
+
+      return new StreamableFile(fileStream);
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 }
