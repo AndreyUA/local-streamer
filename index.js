057d942f48d70c5f4ee1932554b8389847db6883
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-17 20:44:13 +0200

    static content for root url

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
@@ -17,6 +19,15 @@ app.use(cors());
 
 app.use("/api", routes);
 
+// Static content for root URL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.resolve(__dirname + "/dist")));
+app.get("/", (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "/index.html"));
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is started on port ${PORT}`);
 });
