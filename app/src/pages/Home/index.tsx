474ac3895fc11b9cc56b6675d1d1c366f46bc34b
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-19 20:53:00 +0200

    material list in home page

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import TheatersIcon from "@mui/icons-material/Theaters";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
 
 import calculateCurrentUrl from "../../utils/calculateCurrentUrl";
 
 const Home = () => {
   const [videos, setVideos] = useState<Array<String>>([]);
+
+  const navigate = useNavigate();
 
   const getAllVideos = async (): Promise<void> => {
     const response = await axios.get(calculateCurrentUrl());
@@ -13,20 +21,28 @@ const Home = () => {
     setVideos(response.data);
   };
 
+  const navigateToVideo = (videoId: number): void => {
+    navigate(`player/${videoId}`);
+  };
+
   useEffect(() => {
     getAllVideos();
   }, []);
 
+  // TODO: add loading (videos.length === 0 OR videos.length > 0)
   return (
-    <>
-      <ul>
-        {videos.map((video, index) => (
-          <li key={index}>
-            <Link to={`player/${index}`}>{video}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <List>
+      {videos.map((video, index) => (
+        <ListItem key={index}>
+          <ListItemButton onClick={() => navigateToVideo(index)}>
+            <ListItemIcon>
+              <TheatersIcon fontSize="large" />
+            </ListItemIcon>
+            <ListItemText primary={video} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
