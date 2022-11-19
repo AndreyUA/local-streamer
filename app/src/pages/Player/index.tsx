18d439f979e9febc2c9534dc7aef71b8242ca599
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-19 19:18:49 +0200

    styled player

--- app/src/pages/Player/index.tsx
+++ app/src/pages/Player/index.tsx
@@ -1,17 +1,35 @@
 import { useParams } from "react-router-dom";
+import Container from "@mui/material/Container";
+import { styled as materialStyled } from "@mui/material/styles";
+
+import styled from "styled-components";
 
 import calculateCurrentUrl from "../../utils/calculateCurrentUrl";
 
+const StyledContainer = materialStyled(Container)`
+  padding-top: 24px;
+  padding-bottom: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Video = styled.video`
+  width: 100%;
+`;
+
+// TODO: add here title with film name
 const Player = () => {
   const { id } = useParams();
 
   return (
-    <video controls>
-      <source
-        src={`${calculateCurrentUrl()}/stream/${id}`}
-        type="video/mp4"
-      ></source>
-    </video>
+    <StyledContainer>
+      <Video controls>
+        <source
+          src={`${calculateCurrentUrl()}/stream/${id}`}
+          type="video/mp4"
+        ></source>
+      </Video>
+    </StyledContainer>
   );
 };
 
