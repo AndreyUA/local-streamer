c54e3f073843513f8f5b0f7e2a2d37c9afae4cd4
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-19 17:02:00 +0200

    routing, home and player pages

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Player from "./pages/Player";
+
 const App = () => {
-  const calculateCurrentUrl = (): string => {
-    if (process.env.NODE_ENV === "production") {
-      return `${globalThis.location.href}api/stream`;
-    }
-
-    return "http://localhost:3000/api/stream";
-  };
-
   return (
-    <video controls>
-      <source src={calculateCurrentUrl()} type="video/mp4"></source>
-    </video>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="player" element={<Player />} />
+    </Routes>
   );
 };
 
---
+++ app/src/pages/Player.tsx
@@ -0,0 +1,17 @@
+const Player = () => {
+  const calculateCurrentUrl = (): string => {
+    if (process.env.NODE_ENV === "production") {
+      return `${globalThis.location.href}api/stream`;
+    }
+
+    return "http://localhost:3000/api/stream";
+  };
+
+  return (
+    <video controls>
+      <source src={calculateCurrentUrl()} type="video/mp4"></source>
+    </video>
+  );
+};
+
+export default Player;
