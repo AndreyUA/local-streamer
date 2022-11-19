79aa8a666ab805990de3a76a382a79b0ea05365d
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-19 19:20:22 +0200

    added space to header title

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -24,7 +24,7 @@ const Header = () => {
               justifyContent: "center",
             }}
           >
-            Home stream service
+            Home stream service&nbsp;
             <FavoriteIcon />
           </Typography>
         </Toolbar>
