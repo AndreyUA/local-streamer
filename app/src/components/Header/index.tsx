51b071d1e67f7262cdc8c5ea3b2e5a9c6c772e23
Andrii Zhyhariev <infernalsteel666@gmail.com>
2022-11-19 19:31:46 +0200

    homer button in header component

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,19 +1,45 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import IconButton from "@mui/material/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
 
 const StyledBox = styled(Box)`
   flex: 0 1 auto;
 `;
+const HomeButton = styled(IconButton)`
+  position: absolute;
+`;
 
 const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const navigateToHomePage = (): void => {
+    navigate("/");
+  };
+
   return (
     <StyledBox>
       <AppBar position="static">
         <Toolbar>
+          {location.pathname !== "/" && (
+            <HomeButton
+              onClick={navigateToHomePage}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <HomeIcon />
+            </HomeButton>
+          )}
+
           <Typography
             variant="h6"
             component="div"
